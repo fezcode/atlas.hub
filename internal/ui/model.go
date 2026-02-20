c30c51fd87331d5fa9cd8d3aab0af1f70179b06d
fezcode <samil.bulbul@gmail.com>
2026-02-21 00:17:37 +0300

    fix: remove unused key import in ui/model.go

--- internal/ui/model.go
+++ internal/ui/model.go
@@ -5,7 +5,6 @@ import (
 
 	"atlas.hub/internal/install"
 	"atlas.hub/internal/model"
-	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
